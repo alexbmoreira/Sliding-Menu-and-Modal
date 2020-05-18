d5a9ace62797e80d31dfab05e76f7454884cbf12
Alex Moreira <alexbmoreira@gmail.com>
2020-05-18 10:35:11 -0400

    Sign up button opens modal

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const toggle = document.getElementById("toggle");
-const close = document.getElementById("close");
+const close = document.getElementById("close_modal");
 const open = document.getElementById("open");
 const modal = document.getElementById("modal");
 
 toggle.addEventListener("click", function (e)
 {
     document.body.classList.toggle("show_nav");
+});
+
+open.addEventListener("click", function (e)
+{
+    modal.classList.add("show_modal");
+});
+
+close.addEventListener("click", function (e)
+{
+    modal.classList.remove("show_modal");
 });
